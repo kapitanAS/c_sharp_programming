550907e41677ade6ce069102b33bd3869b4f04cf
kapitanAS <jacek.rosiecki@yahoo.co.uk>
2015-09-30 22:52:37 +0100

    added Customer

---
+++ linq_queries/linq_queries/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_queries
+{
+   class Customer: ICustomer
+   {
+      string Name { get; set; }
+      string Address { get; set; }
+
+      public Customer(string name, string address)
+      {
+         Name = name;
+         Address = address;
+      }
+
+      public string GetName()
+      {
+         return Name;
+      }
+
+      public string GetAddress()
+      {
+         return Address;
+      }
+   }
+}
