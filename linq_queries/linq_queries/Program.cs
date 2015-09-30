c17dba946f5505970df7f6abe6e9a5bdc8e9d51b
kapitanAS <jacek.rosiecki@yahoo.co.uk>
2015-09-30 22:26:25 +0100

    created new project - linq_queries

---
+++ linq_queries/linq_queries/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_queries
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
